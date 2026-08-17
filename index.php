f1d5ff947629f2291f4e6aedfd54e0b3334fb44d
XinYueNeko <3649745663@qq.com>
2026-08-17 09:47:21 +0800

    Update

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <div class="container">
         <h1>PHP写的网站</h1>
         <?php
-        echo "<p class='p_size'>新月的纯PHP Website</p><br>";
-        echo "<p class='p_size'>下面是一些链接</p><br>"; # 我很懒，我连介绍都懒得写
+            echo "<p class='p_size'>\"新月猫Neko\"的纯PHP Website</p><br>";
+            echo "<p class='p_size'>下面是一些链接</p><br>"; # 我很懒，我连介绍都懒得写
         # 我是懒狗
         ?>
         <a href='mms.php', class='button'>MaoMaoPE</a><!-- 跳转到猫猫服务器的页面  -->
