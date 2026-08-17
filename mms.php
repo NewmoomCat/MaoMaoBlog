9de860035b6146bd29432c84bb6c97ed14f71b4a
XinYueNeko <3649745663@qq.com>
2026-08-17 10:42:39 +0800

    修改一些mms相关的

--- mms.php
+++ mms.php
@@ -9,13 +9,15 @@
 <body>
     <div class="container">
         <h1 class="top-title">MaoMaoPE服务器</h1>
-        <h2>服务器介绍</h2>
-        <?php
-            echo "<p class='p_size'>猫猫怀旧休闲服，一个主打怀旧休闲的服务器！</p><br>";
-            echo "<p class='p_size'>服务器最初是做高版本1.21.x。后来\"新月猫Neko\"想试试开低版本怀旧服务器，也想回忆回忆过去的童年，于是便有了MaoMaoPE</p><br>";
-            echo "<p class='p_size'>服务器用的是我们团队的MatchaCake(闭源的), 支持0.16.x ~ 1.1.x的版本游玩</p><br>";
-            echo "<p class='p_size'>(其实搞跨版本支持最初是为了装逼的，直到我们把0.16.x ~ 1.0.x搞出来...到后面变得一发不可收拾，直接支持到0.10 ~ 1.1)</p><br>";
-        ?>
+        <div class="terminal-box">
+            <h2 class="terminal-title">服务器介绍</h2>
+            <?php
+                echo "<p class='p_size'>猫猫怀旧休闲服，一个主打怀旧休闲的服务器！</p><br>";
+                echo "<p class='p_size'>服务器最初是做高版本1.21.x。后来\"新月猫Neko\"想试试开低版本怀旧服务器，也想回忆回忆过去的童年，于是便有了MaoMaoPE</p><br>";
+                echo "<p class='p_size'>服务器用的是我们团队的MatchaCake(闭源的), 支持0.16.x ~ 1.1.x的版本游玩</p><br>";
+                echo "<p class='p_size'>(其实搞跨版本支持最初是为了装逼的，直到我们把0.16.x ~ 1.0.x搞出来...到后面变得一发不可收拾，直接支持到0.10 ~ 1.1)</p><br>";
+            ?>
+        </div>
         <a href="index.php", class="button">返回上一页</a>
         <a href="https://qm.qq.com/q/HBv5WcpGk8", class="button">加入QQ群</a>
         <!-- <a href="minecraft://?addExternalServer=服务器名称|服务器地址:端口">test</a> -->
